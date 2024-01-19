15ed54c032170eef6c89637e19e637f6955cb2a2
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-01-19 09:40:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ function handleInput(input, event) {
+            if (event.inputType === "deleteContentBackward" && input.previousElementSibling) {
+                input.previousElementSibling.focus();
+            } else if (event.inputType !== "deleteContentBackward" && input.nextElementSibling) {
+                input.nextElementSibling.focus();
+            }
+        }
